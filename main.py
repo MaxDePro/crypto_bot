6acc631d9bdf55035fa1489b755e73ce4dc8bcfc
MaxDePro <xmaksg@gmail.com>
2022-10-17 21:59:15 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from tokens import cmc_token
+import requests
+
+
+def get_cmc_data(crypto):
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ tokens.py
@@ -0,0 +1,2 @@
+cmc_token = 'd08ef4c1-e2a7-4da4-849d-127338dee4d9'
+
