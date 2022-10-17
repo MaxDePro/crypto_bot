1153a50cead94c981a4e8c7f9ae5673ec5ed8697
MaxDePro <xmaksg@gmail.com>
2022-10-17 22:26:30 +0300

    realize function write_to_json  where we wrete our data to json file and modify function get_cmc_data where we taken price of our coin in USD

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 from tokens import cmc_token
+import json
 import requests
+
+
+def write_json(data, filename='response.json'):
+    with open(filename, 'w') as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
 
 
 def get_cmc_data(crypto):
     url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+    params = {
+        'symbol': crypto,
+        'convert': 'USD'
+    }
+    headers = {'X-CMC_PRO_API_KEY': cmc_token}
+
+    r = requests.get(url, headers=headers, params=params).json()
+
+    price = r['data'][crypto]['quote']['USD']['price']
+    print(price)
+
 
 def main():
-    pass
+    get_cmc_data('BTC')
+
 
 if __name__ == '__main__':
     main()
